a8fffd0e5b73ba465d4ce59defa850543c8ace9c
trieuphan0211 <trieu.207ct28659@vanlanguni.vn>
2023-07-27 21:36:10 +0700

    deploy fix

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ app.use('/api/customer', require('./api/customer.js'));
 // deployment
 const path = require('path');
 // '/admin' serve the files at client-admin/build/* as static files
-app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.use('/admin', express.static(path.resolve(__dirname, '../client_admin/build')));
 app.get('admin/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client_admin/build', 'index.html'));
 });
 // '/' serve the files at client-customer/build/* as static files
-app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.use('/', express.static(path.resolve(__dirname, '../client_customer/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
 });
